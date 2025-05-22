8ffbee15cb6162dfd5aadda7d1411810279ee0c9
n1346046 <n1346046@my.ntu.ac.uk>
2025-05-22 14:52:45 +0100

    added PostDbConnectionFactory for creating connection Object

---
+++ Data/Factories/PostDbConnectionFactory.cs
@@ -0,0 +1,21 @@
+using System.Data;
+using Npgsql;
+
+namespace WatchDog.Data.Factories;
+
+public class PostDbConnectionFactory
+{
+    private readonly string _connectionString;
+    
+    public PostDbConnectionFactory(string connectionString)
+    {
+       this._connectionString = connectionString;
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        var connection = new NpgsqlConnection(this._connectionString);
+        connection.Open();
+        return connection;
+    }
+}
