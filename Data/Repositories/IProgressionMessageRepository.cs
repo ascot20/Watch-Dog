e803089ab8b2c15001770a537527958364218aef
n1346046 <n1346046@my.ntu.ac.uk>
2025-05-24 17:19:24 +0100

    modified IProgressionMessageRepository

--- Data/Repositories/IProgressionMessageRepository.cs
+++ Data/Repositories/IProgressionMessageRepository.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WatchDog.Models;
+
 namespace WatchDog.Data.Repositories;
 
-public interface IProgressionMessageRepository
+public interface IProgressionMessageRepository: IRepository<ProgressionMessage>
 {
-    
+    Task<IEnumerable<ProgressionMessage>> GetMessagesBySubTaskIdAsync(int subTaskId);
+    Task<IEnumerable<ProgressionMessage>> GetMessagesByCreatorIdAsync(int creatorId);
+    Task<ProgressionMessage?> GetMessageWithCreatorAsync(int messageId);
+    Task<ProgressionMessage?> GetMessageWithSubTaskAsync(int messageId);
+    Task<int> GetTotalMessageCountForSubTaskAsync(int subTaskId);
 }
